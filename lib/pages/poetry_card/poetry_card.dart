a9344856126db76a409a0ed974b1344ce1c42955
Archer-Min <13989302690@163.com>
2023-09-27 01:18:59 +0800

    feat: 点击诗词更换卡片；保存卡片到相册

--- lib/pages/poetry_card/poetry_card.dart
+++ lib/pages/poetry_card/poetry_card.dart
@@ -1,9 +1,13 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:whispers_of_tea/constant/app_assets.dart';
 import 'package:whispers_of_tea/constant/app_theme.dart';
 import 'package:whispers_of_tea/constant/poetry.dart';
+
+import '../../constant/app_style.dart';
 
 class PoetryCardPage extends StatefulWidget {
   const PoetryCardPage({super.key});
@@ -30,6 +34,15 @@ class _PoetryCardPageState extends State<PoetryCardPage> {
     super.initState();
   }
 
+  ///更换诗词卡片
+  refreshCard(){
+    setState(() {
+      currCard = currCard%_cardNumber+1;
+      title = Poetry.poetryList[currCard - 1]['title'] ?? '';
+      content = Poetry.poetryList[currCard - 1]['content'] ?? '';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,8 +50,15 @@ class _PoetryCardPageState extends State<PoetryCardPage> {
         children: [
           _getBackground(),
           Padding(
-            padding: const EdgeInsets.fromLTRB(30, 100, 30, 100),
+            padding: const EdgeInsets.fromLTRB(30, 80, 30, 110),
             child: _getCard(),
+          ),
+          Positioned(
+            bottom: 30,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10,left: 30,right: 30),
+              child: _storeCard(),
+            ),
           ),
         ],
       ),
@@ -73,10 +93,59 @@ class _PoetryCardPageState extends State<PoetryCardPage> {
       ),
       child: Column(
         children: [
-          Text(title, textAlign: TextAlign.center),
-          Text(content, textAlign: TextAlign.center),
+          Text(title, textAlign: TextAlign.center,style: AppStyle.poetryTitleStyle,),
+          GestureDetector(
+              onTap: refreshCard,
+              child: Text(content, textAlign: TextAlign.center,style: AppStyle.poetryStyle,)),
         ],
       ),
     );
+  }
+
+  ///保存到手机
+  _storeCard(){
+    return GestureDetector(
+      onTap: (){
+        _saveLocalImage();
+      },
+      child: Container(
+        width: 349,
+        height: 58,
+        decoration: BoxDecoration(
+          color: AppTheme.poetryCardBgColor,
+          borderRadius: const BorderRadius.all(Radius.circular(33)),
+        ),
+
+        child: Center(
+          child: Text("保存到手机",style: TextStyle(fontSize: 20),),
+        ),
+
+      ),
+    );
+  }
+
+  ///保存卡片到相册方法
+  Future<void> _saveLocalImage() async {
+    // 构建图片的路径
+    String imagePath = '${AppAssets.poetryCardPath}/${currCard}_poetry_card.png';
+
+    try {
+      // 从资源中加载图片
+      ByteData byteData = await rootBundle.load(imagePath);
+
+      // 将ByteData转换为Uint8List
+      Uint8List uint8List = byteData.buffer.asUint8List();
+
+      // 将图片保存到图库
+      final result = await ImageGallerySaver.saveImage(uint8List);
+
+      if (result['isSuccess']) {
+        print('图片保存成功');
+      } else {
+        print('图片保存失败');
+      }
+    } catch (e) {
+      print('发生错误: $e');
+    }
   }
 }
