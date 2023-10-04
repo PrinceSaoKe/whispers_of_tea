c1aca6f502c9aa0ea2a754cc3c88a56f273db4b7
Archer-Min <13989302690@163.com>
2023-10-05 00:57:02 +0800

    feat: 完成泡茶过程页面

---
+++ lib/pages/brew_tea_step/brew_tea.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:whispers_of_tea/constant/app_assets.dart';
+import 'package:whispers_of_tea/constant/app_style.dart';
+import 'package:whispers_of_tea/constant/app_theme.dart';
+import 'package:whispers_of_tea/constant/make_tea_step.dart';
+
+import '../../constant/brew_tea_step.dart';
+
+class BrewTeaPage extends StatefulWidget {
+  const BrewTeaPage({super.key});
+
+  @override
+  State<BrewTeaPage> createState() => _BrewTeaPageState();
+}
+
+class _BrewTeaPageState extends State<BrewTeaPage> {
+  int currStep = 0;
+  String title = '';
+  String content = '';
+
+  /// 切换泡茶步骤
+  _getStepContent(int step) {
+    currStep = step;
+    title = BrewTeaStep.brewTeaStep[currStep]['title'] ?? '';
+    content = BrewTeaStep.brewTeaStep[currStep]['text'] ?? '';
+  }
+
+  @override
+  void initState() {
+    _getStepContent(0);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (currStep < BrewTeaStep.brewTeaStep.length - 1) {
+              _getStepContent(currStep + 1);
+            }
+          });
+        },
+        child: Stack(
+          children: [
+            _getBackground(),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  const SizedBox(height: 40),
+                  _getTitle(),
+                  SizedBox(height: 10,),
+                  _getContentRect(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// 背景
+  _getBackground() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppAssets.brewTeaStepBg),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  /// 标题
+  _getTitle() {
+    return Container(
+      width: 128,
+      height: 128,
+      alignment: Alignment.center,
+      child: Text(title, style: AppStyle.brewTeaTitleStyle),
+    );
+  }
+
+  /// 文本框
+  _getContentRect() {
+    return Container(
+      width: 365,
+      height: 128,
+      padding: const EdgeInsets.fromLTRB(35, 20, 35, 20),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppAssets.brewTeaStepBorder),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Text(content, style: AppStyle.brewTeaTextStyle)
+    );
+  }
+}
---
+++ lib/res/assets_res.dart
@@ -0,0 +1,11 @@
+/// Generated file. Do not edit.
+
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class AssetsRes {
+  static const String PLUGIN_NAME = 'whispers_of_tea';
+  static const String PLUGIN_VERSION = '1.0.0+1';
+  static const String BREW_TEA_STEP_BORDER = 'assets/images/brew_tea_step/brew_tea_step_border.png';
+  static const String LEADING = 'assets/images/leading.png';
+  static const String NAV_TREE = 'assets/images/nav_tree.png';
+}
